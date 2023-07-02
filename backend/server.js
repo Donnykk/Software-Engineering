80c7662610f65d58b3660daae568d0088fa4694d
cliffwalker <q18642256190@163.com>
2023-07-02 20:35:16 +0800

    unstable function1.1

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,35 @@ router.post('/api/submit', async (ctx) => {
     }
   });
 
+  router.get('/api/questions/:examId', async (ctx) => {
+    try {
+        const examId = ctx.params.examId;
+
+        console.log('Received examId:', examId);
+
+        // 连接到MySQL数据库
+        const connection = await mysql.createConnection({
+        host: 'localhost', // 修改为你的数据库主机名
+        user: 'root', // 修改为你的数据库用户名
+        password: 'Top50761', // 修改为你的数据库密码
+        database: 'test-sql' // 修改为你的数据库名称
+    });
+
+    // 执行查询语句获取考题内容
+    sql = 'SELECT * FROM a_questions WHERE exam_entry_id = ?'
+    const rows = await connection.execute(sql, [examId]);
+
+    console.log(sql);
+
+    connection.end(); // 关闭数据库连接
+
+    ctx.body = { data: rows }; // 将查询结果作为JSON响应发送给前端
+    } catch (error) {
+    console.error(error);
+    ctx.status = 500;
+    ctx.body = { error: 'Internal server error' };
+    }
+});
 app.use(router.routes());
 
 
